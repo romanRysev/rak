6aac697df79d39f1c795f2ca41d79f9034e7099c
ncaatl <alncaatl@gmail.com>
2020-05-18 12:52:48 +0300

    полоски вокруг 'поделитесь историей' на странице конкретной истории

--- pages/stories/_id.vue
+++ pages/stories/_id.vue
@@ -32,52 +32,58 @@
       </div>
     </container>
 
-    <article class="story-article">
-      <p class="story-article story-article__paragraph">
-        Я из военной семьи. Отец хоть и не был военным сам, но нас всех держал в
-        ежовых рукавицах. Думаю, поэтому мы и выросли такими ответственными.
-      </p>
-      <p class="story-article story-article__paragraph">
-        У меня дома до сих пор стоят часы в каждой комнате, хотя они и не нужны
-        особо — я сам чувствую, опаздываю куда-то или нет, отстаю от нужного
-        графика или опережаю. Вот такие встроенные внутренние часы! Будильник
-        мне тоже не нужен — я всегда встаю раньше. Одеваюсь тоже быстро, как в
-        армии, за 45 секунд.
-      </p>
-      <p class="story-article story-article__paragraph">
-        <span class="story-article__paragraph semi-bold"
-          >«В футболе если команда опоздала на 15 минут, ей засчитывается
-          поражение».</span
-        >
-      </p>
-      <p class="story-article story-article__paragraph">
-        Опаздывать я тоже не люблю, на все встречи прихожу заранее. Если знаю,
-        что могу попасть по дороге в пробку, то не еду на машине. В аэропорт
-        приезжаю задолго до начала регистрации. Лучше подожду и кофе попью, чем
-        опоздаю!
-      </p>
-      <p class="story-article story-article__paragraph">
-        Когда мне было 16 лет, мне в школе геометрию нужно было пересдавать. Я
-        билеты выучил, знал абсолютно все. Пришел в нужное время, а учительница
-        — нет. Ну, я какое-то время подождал ее и ушел. Потом она спрашивала:
-        «Почему не дождался?». Я ответил: «В футболе если команда опоздала на 15
-        минут, ей засчитывается поражение». Экзамен мне все-таки поставили!
-        Сейчас если кто-то из футболистов моей команды опаздывает — начинаю
-        злиться, могу и прикрикнуть потом. А если кто-то опоздал на тренировку
-        перед игрой — все, подготовка насмарку. Я сразу начинаю думать тогда:
-        «Значит, точно проиграем». Такая болезненная пунктуальность уже не
-        лечится. В отличие от рака.
-      </p>
-      <p class="story-article story-article__paragraph">
-        «Сейчас если кто-то из футболистов моей команды опаздывает — начинаю
-        злиться, могу и прикрикнуть потом. А если кто-то опоздал на тренировку
-        перед игрой — все, подготовка насмарку. Я сразу начинаю думать тогда:
-        «Значит, точно проиграем». Такая болезненная пунктуальность уже не
-        лечится».
-      </p>
-    </article>
+    <container class="container container_article">
+      <article class="story-article">
+        <p class="story-article story-article__paragraph">
+          Я из военной семьи. Отец хоть и не был военным сам, но нас всех держал
+          в ежовых рукавицах. Думаю, поэтому мы и выросли такими ответственными.
+        </p>
+        <p class="story-article story-article__paragraph">
+          У меня дома до сих пор стоят часы в каждой комнате, хотя они и не
+          нужны особо — я сам чувствую, опаздываю куда-то или нет, отстаю от
+          нужного графика или опережаю. Вот такие встроенные внутренние часы!
+          Будильник мне тоже не нужен — я всегда встаю раньше. Одеваюсь тоже
+          быстро, как в армии, за 45 секунд.
+        </p>
+        <p class="story-article story-article__paragraph">
+          <span class="story-article__paragraph semi-bold"
+            >«В футболе если команда опоздала на 15 минут, ей засчитывается
+            поражение».</span
+          >
+        </p>
+        <p class="story-article story-article__paragraph">
+          Опаздывать я тоже не люблю, на все встречи прихожу заранее. Если знаю,
+          что могу попасть по дороге в пробку, то не еду на машине. В аэропорт
+          приезжаю задолго до начала регистрации. Лучше подожду и кофе попью,
+          чем опоздаю!
+        </p>
+        <p class="story-article story-article__paragraph">
+          Когда мне было 16 лет, мне в школе геометрию нужно было пересдавать. Я
+          билеты выучил, знал абсолютно все. Пришел в нужное время, а
+          учительница — нет. Ну, я какое-то время подождал ее и ушел. Потом она
+          спрашивала: «Почему не дождался?». Я ответил: «В футболе если команда
+          опоздала на 15 минут, ей засчитывается поражение». Экзамен мне
+          все-таки поставили! Сейчас если кто-то из футболистов моей команды
+          опаздывает — начинаю злиться, могу и прикрикнуть потом. А если кто-то
+          опоздал на тренировку перед игрой — все, подготовка насмарку. Я сразу
+          начинаю думать тогда: «Значит, точно проиграем». Такая болезненная
+          пунктуальность уже не лечится. В отличие от рака.
+        </p>
+        <p class="story-article story-article__paragraph">
+          «Сейчас если кто-то из футболистов моей команды опаздывает — начинаю
+          злиться, могу и прикрикнуть потом. А если кто-то опоздал на тренировку
+          перед игрой — все, подготовка насмарку. Я сразу начинаю думать тогда:
+          «Значит, точно проиграем». Такая болезненная пунктуальность уже не
+          лечится».
+        </p>
+      </article>
 
-    <share-link class="share-link" :url="links[1].url" :text="links[1].text" />
+      <share-link
+        class="share-link share-link_article"
+        :url="links[1].url"
+        :text="links[1].text"
+      />
+    </container>
 
     <container>
       <story-grid class="story-grid" />
@@ -188,10 +194,13 @@ export default {
   padding: 0;
 }
 
-.story-article {
-  padding: 130px 0 100px;
+.container_article {
   max-width: 780px;
+  padding: 130px 0 0 0;
   margin: 0 auto;
+}
+
+.story-article {
   font-size: 22px;
   line-height: 30px;
 }
@@ -208,8 +217,21 @@ export default {
   color: #121212;
 }
 
+.share-link_article {
+  width: 100%;
+  border-top: 1px solid #efefef;
+  border-bottom: 1px solid #efefef;
+  height: 84px;
+  display: inline-grid;
+  align-items: center;
+  text-align: center;
+  margin-top: 70px;
+  font-size: 18px;
+  line-height: 24px;
+}
+
 .story-grid {
-  margin-top: 190px;
+  margin-top: 160px;
 }
 
 .more-button {
@@ -234,11 +256,14 @@ export default {
     line-height: 44px;
   }
 
+  .container_article {
+    max-width: 720px;
+    padding: 120px 0 0 0;
+  }
+
   .story-article {
     font-size: 20px;
     line-height: 28px;
-    max-width: 720px;
-    padding: 120px 0 80px;
   }
 
   .story-article__paragraph {
@@ -251,7 +276,7 @@ export default {
   }
 
   .story-grid {
-    margin: 180px 0 60px;
+    margin: 150px 0 60px;
   }
 
   .more-button {
@@ -284,11 +309,14 @@ export default {
     margin-bottom: 15px;
   }
 
+  .container_article {
+    max-width: 640px;
+    padding: 90px 0 0 0;
+  }
+
   .story-article {
     font-size: 18px;
     line-height: 27px;
-    max-width: 640px;
-    padding: 90px 0 70px;
   }
 
   .story-article__paragraph {
@@ -304,8 +332,15 @@ export default {
     line-height: 22px;
   }
 
+  .share-link_article {
+    font-size: 16px;
+    line-height: 22px;
+    height: 70px;
+    margin: 50px 0 0 0;
+  }
+
   .story-grid {
-    margin: 140px 0 45px;
+    margin: 120px 0 45px;
   }
 
   .more-button {
@@ -349,11 +384,14 @@ export default {
     margin-bottom: 15px;
   }
 
+  .container_article {
+    max-width: 640px;
+    padding: 100px 0 0 0;
+  }
+
   .story-article {
     font-size: 18px;
     line-height: 27px;
-    max-width: 640px;
-    padding: 100px 0;
   }
 
   .story-article__paragraph {
@@ -369,8 +407,15 @@ export default {
     line-height: 22px;
   }
 
+  .share-link_article {
+    font-size: 16px;
+    line-height: 22px;
+    height: 70px;
+    margin: 80px 0 0 0;
+  }
+
   .story-grid {
-    margin: 145px 0 40px;
+    margin: 120px 0 40px;
   }
 
   .more-button {
@@ -414,10 +459,14 @@ export default {
     margin-bottom: 15px;
   }
 
+  .container_article {
+    max-width: 640px;
+    padding: 40px 0 0 0;
+  }
+
   .story-article {
     font-size: 13px;
     line-height: 16px;
-    padding: 40px 0 60px;
   }
 
   .story-article__paragraph {
@@ -434,8 +483,15 @@ export default {
     text-align: center;
   }
 
+  .share-link_article {
+    font-size: 13px;
+    line-height: 16px;
+    height: 74px;
+    margin: 40px 0 0 0;
+  }
+
   .story-grid {
-    margin: 125px 0 40px;
+    margin: 100px 0 40px;
   }
 
   .more-button {
