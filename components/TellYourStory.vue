f87b5becc7b13fee1e48c5bba8d65df5df5294fd
AleksandrHexlet <46378431+AleksandrHexlet@users.noreply.github.com>
2020-05-20 18:23:24 +0300

    commit

--- components/TellYourStory.vue
+++ components/TellYourStory.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="your-story">
-    <div class="content-container">
+    <container class="container container_your-story">
       <h3 class="your-story-header">Расскажите свою историю</h3>
       <div class="description-container">
         <div class="description-side">
@@ -10,38 +10,97 @@
             навязчивых идей и болезненных привязанностей.
           </p>
           <div class="variant-toggler">
-            <p class="variant variant_active">1-й вариант</p>
-            <p class="variant">2-й вариант</p>
+            <p
+              ref="variantOne"
+              @click="variantToggler"
+              class="variant variant_active"
+            >
+              1-й вариант
+            </p>
+            <p ref="variantTwo" @click="variantToggler" class="variant">
+              2-й вариант
+            </p>
           </div>
         </div>
         <div class="variant-side">
-          <p class="variant-text">
+          <p v-if="variantOneShown" class="variant-text">
             Заполнить подробную форму прямо на сайте и мы опубликуем вашу
             историю после проверки. Пожалуйста, заполняйте все пункты корректно,
             если вы испытаете какие-то сложности, воспользуйтесь 2&#8209;м
             вариантом.
           </p>
-          <!--            <p class="variant-text">Оставить контакт (почту или номер телефона) и мы свяжемся с вами, зададим вопросы, уточним детали вашей истории.</p>-->
+          <p v-if="variantTwoShown" class="variant-text">
+            Оставить контакт (почту или номер телефона) и мы свяжемся с вами,
+            зададим вопросы, уточним детали вашей истории.
+          </p>
         </div>
       </div>
 
-      <Button
+      <!--<Button
+        v-if="variantOneShown"
         @custom-click="$store.commit('popup/open')"
         class="form-button"
         type="button"
         >Заполнить форму</Button
       >
-    </div>
+
+      <Button
+        v-if="variantTwoShown"
+        @custom-click="$store.commit('popup/open')"
+        class="form-button"
+        type="button"
+        >Оставить контакт</Button
+      >-->
+
+      <popupbutton
+        v-if="variantOneShown"
+        @custom-click="$store.commit('popup/open')"
+        className="popup__button"
+        class="form-button"
+      >
+        <p class="popup__button-description">
+          <slot>Заполнить форму</slot>
+        </p>
+      </popupbutton>
+
+      <popupbutton
+        v-if="variantTwoShown"
+        @custom-click="$store.commit('popup/open')"
+        className="popup__button"
+        class="form-button"
+      >
+        <p class="popup__button-description">
+          <slot>Оставить контакт</slot>
+        </p>
+      </popupbutton>
+    </container>
   </section>
 </template>
 
 <script>
+import Container from '~/components/Container';
 import button from '@/components/button';
 export default {
   components: {
-    Button: button,
+    popupbutton: button,
+    container: Container,
   },
-  name: 'TellYourStory',
+  methods: {
+    variantToggler(e) {
+      if (!e.target.classList.contains('variant_active')) {
+        this.variantOneShown = !this.variantOneShown;
+        this.variantTwoShown = !this.variantTwoShown;
+        this.$refs.variantOne.classList.toggle('variant_active');
+        this.$refs.variantTwo.classList.toggle('variant_active');
+      }
+    },
+  },
+  data() {
+    return {
+      variantOneShown: true,
+      variantTwoShown: false,
+    };
+  },
 };
 </script>
 
@@ -55,11 +114,6 @@ export default {
   color: #666666;
   padding: 100px 0;
   position: relative;
-}
-
-.content-container {
-  max-width: 1320px;
-  margin: 0 auto;
 }
 
 .your-story-header {
@@ -89,28 +143,31 @@ export default {
 }
 
 .description-text {
-  width: 55%;
+  width: 340px;
 }
 
 .variant-toggler {
   text-align: right;
-  min-width: 110px;
-  width: 21%;
+  width: 110px;
   line-height: 1.7em;
 }
 
 .variant {
-  /* Оставил пока пустым. Пока не уверен, как именно здесь будет выглядеть переключение отображаемых абзацев.
-    Если модификатором, то нужен и блок. Если без мдоификатора, то нужно будет слегка изменить. */
+  cursor: pointer;
+}
+
+.variant:hover {
+  color: black;
 }
 
 .variant_active {
   font-weight: 500;
   color: black;
+  cursor: default;
 }
 
 .form-button {
-  width: 20%;
+  width: 280px;
   max-width: 280px;
   min-width: 180px;
   height: 52px;
@@ -121,15 +178,16 @@ export default {
   line-height: 19px;
   position: absolute;
   left: calc(50% + 20px);
+  cursor: pointer;
+}
+
+.form-button:hover {
+  opacity: 0.9;
 }
 
 @media screen and (min-width: 1280px) and (max-width: 1439px) {
   .your-story {
     padding: 90px 0;
-  }
-
-  .content-container {
-    max-width: 1180px;
   }
 
   .your-story-header {
@@ -145,7 +203,7 @@ export default {
   .description-text {
     font-size: 16px;
     line-height: 20px;
-    width: 54%;
+    width: 305px;
   }
 
   .variant-toggler {
@@ -154,7 +212,7 @@ export default {
 
   .form-button {
     height: 48px;
-    width: 18%;
+    width: 230px;
   }
 }
 
@@ -163,11 +221,6 @@ export default {
     font-size: 13px;
     line-height: 16px;
     padding: 75px 0;
-  }
-
-  .content-container {
-    width: 90%;
-    max-width: 924px;
   }
 
   .your-story-header {
@@ -182,12 +235,16 @@ export default {
   }
 
   .description-text {
-    width: 60%;
+    width: 260px;
   }
 
   .variant-toggler {
     font-size: 15px;
     line-height: 23px;
+  }
+
+  .variant-side {
+    width: calc(50% - 15px);
   }
 
   .variant-text {
@@ -197,14 +254,20 @@ export default {
 
   .form-button {
     height: 46px;
-    width: 23%;
-    left: calc(50% + 14px);
+    width: 230px;
+    left: calc(50% + 15px);
     font-size: 15px;
     line-height: 18px;
   }
 }
 
 @media screen and (min-width: 730px) and (max-width: 1022px) {
+  .container_your-story {
+    width: 380px;
+    padding: 0;
+    align-items: center;
+  }
+
   .your-story {
     font-size: 15px;
     line-height: 19px;
@@ -215,18 +278,12 @@ export default {
     font-size: 24px;
     line-height: 28px;
     text-align: center;
-  }
-
-  .content-container {
-    width: 53%;
-    max-width: 688px;
+    width: 100%;
   }
 
   .description-container {
     display: flex;
     flex-direction: column;
-    margin-left: 12px;
-    width: 95%;
     margin-bottom: 45px;
   }
 
@@ -259,93 +316,28 @@ export default {
 
   .variant-side {
     width: 100%;
+    min-height: 95px;
   }
 
   .form-button {
     position: inherit;
-    margin-left: 12px;
-    width: 57%;
+    width: 230px;
+    height: 46px;
     font-size: 15px;
     line-height: 18px;
-    height: 50px;
   }
 }
 
-@media screen and (min-width: 400px) and (max-width: 729px) {
-  .your-story {
-    font-size: 15px;
-    line-height: 19px;
-    padding: 60px 0;
+@media screen and (max-width: 729px) {
+  .container_your-story {
+    width: 290px;
+    padding: 0;
   }
 
-  .your-story-header {
-    font-size: 24px;
-    line-height: 28px;
-    max-width: 85%;
-  }
-
-  .content-container {
-    width: 80%;
-    max-width: 688px;
-  }
-
-  .description-container {
-    display: flex;
-    flex-direction: column;
-    width: 85%;
-    margin-bottom: 45px;
-  }
-
-  .description-side {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-  }
-
-  .description-text {
-    width: 100%;
-    font-size: 13px;
-    line-height: 16px;
-  }
-
-  .variant-toggler {
-    width: 100%;
-    display: flex;
-    margin: 55px 0 20px;
-    line-height: 1.8em;
-  }
-
-  .variant:not(:last-of-type) {
-    margin-right: 30px;
-  }
-
-  .variant_active {
-    border-bottom: 2px solid currentColor;
-  }
-
-  .variant-side {
-    width: 100%;
-  }
-
-  .form-button {
-    position: inherit;
-    width: 57%;
-    font-size: 15px;
-    line-height: 18px;
-    height: 50px;
-  }
-}
-
-@media screen and (max-width: 399px) {
   .your-story {
     font-size: 13px;
     line-height: 16px;
     padding: 45px 0 50px;
-  }
-
-  .content-container {
-    width: 90%;
-    min-width: 240px;
   }
 
   .your-story-header {
@@ -385,6 +377,7 @@ export default {
 
   .variant-side {
     width: 100%;
+    min-height: 95px;
   }
 
   .form-button {
